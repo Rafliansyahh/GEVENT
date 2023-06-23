00809194d7b0cca3f55c29c3e1dcb494bdbf4bab
Rafliansyah <rafli.ansyahh@mhs.mdp.ac.id>
2023-06-23 18:05:18 +0700

    update

---
+++ app/src/main/java/pab2/uas/rafliansyah/gevent/RegisterActivity.java
@@ -0,0 +1,15 @@
+
+package pab2.uas.rafliansyah.gevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
